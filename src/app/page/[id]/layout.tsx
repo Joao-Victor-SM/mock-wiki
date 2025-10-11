5b0951941ad787e4804f62666900c233f59aca36
Zero_Null <boott3r@gmail.com>
2025-10-11 13:24:01 -0400

    add aside

--- src/app/page/[id]/layout.tsx
+++ src/app/page/[id]/layout.tsx
@@ -1,23 +1,11 @@
 import {ReactNode} from 'react';
-import RecommendationCard from './components/RecommendationCard';
+import Aside from './components/Aside';
 
 export default function Layout({children}: {children: ReactNode}) {
   return (
     <main className="flex gap-4 p-4 sm:px-24 h-full bg-neutral-300 font-mono text-sm flex-col sm:flex-row">
-      <section>{children}</section>
-
-      <aside className="w-80 border border-gray-400 bg-gradient-to-b from-gray-100 to-gray-200 p-1 shadow-md grid content-start gap-1">
-        <RecommendationCard
-          href="132"
-          title="House Locke"
-          content='Prototype soldiers inspired on the "tabula rasa" philosophy'
-        />
-        <RecommendationCard
-          href="1323"
-          title="Another Article"
-          content="Lorem ipsum dolor sit amet"
-        />
-      </aside>
+      {children}
+      <Aside />
     </main>
   );
 }
