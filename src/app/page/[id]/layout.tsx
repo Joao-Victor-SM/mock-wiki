6336c53f6ed65a7031f73896d416c13365a172dc
Zero_Null <boott3r@gmail.com>
2025-10-11 22:42:19 -0400

    aside on left

--- src/app/page/[id]/layout.tsx
+++ src/app/page/[id]/layout.tsx
@@ -4,8 +4,8 @@ import Aside from './components/Aside';
 export default function Layout({children}: {children: ReactNode}) {
   return (
     <main className="flex gap-4 p-4 sm:px-24 h-full bg-neutral-300 font-mono text-sm flex-col sm:flex-row">
-      {children}
       <Aside />
+      {children}
     </main>
   );
 }
