09fd7ef4459c69a3790aad97af3311c2b5ab43c5
Zero_Null <boott3r@gmail.com>
2025-10-11 21:58:29 -0400

    teste fonte

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,12 +1,12 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 const Header = () => {
   return (
     <header className="bg-gray-200 flex p-4 sm:px-24 items-center border-b">
-      <Link href="/" className="cursor-pointer select-none">
-        <h1 className="text-3xl border-b-2 border-l-2 border-green-700 text-gray-950 p-2 py-1 rounded-b-sm !rounded-r-none">
-          New Leaf
-        </h1>
+      <Link href="/" className="cursor-pointer select-none flex gap-2 logofont">
+        <Image src="/Logo_New_Leaf.png" alt="logo" width={50} height={50} />
+        <h1 className="text-3xl  text-gray-950 p-2 py-1 ">New Leaf</h1>
       </Link>
       <nav className="flex ml-auto divide-x">
         <Link href="/" className="text-blue-500 hover:underline px-1.5">
