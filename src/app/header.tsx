2f7d44e522ab7bcc5403f9574587e59055fd7e88
Zero_Null <boott3r@gmail.com>
2025-10-11 22:09:04 -0400

    font

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -4,7 +4,10 @@ import Link from 'next/link';
 const Header = () => {
   return (
     <header className="bg-gray-200 flex p-4 sm:px-24 items-center border-b">
-      <Link href="/" className="cursor-pointer select-none flex gap-2 logofont">
+      <Link
+        href="/"
+        className="cursor-pointer select-none flex gap-[3px] almendra-bold-italic items-center"
+      >
         <Image src="/Logo_New_Leaf.png" alt="logo" width={50} height={50} />
         <h1 className="text-3xl  text-gray-950 p-2 py-1 ">New Leaf</h1>
       </Link>
