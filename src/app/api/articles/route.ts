86379f7d9af75c29e58f53f98cdcb61a78bc72bc
Zero_Null <boott3r@gmail.com>
2025-10-12 12:18:33 -0400

    add route

---
+++ src/app/api/articles/route.ts
@@ -0,0 +1,25 @@
+import {CardSectionInterface} from '@/app/types/Cards';
+import {prisma} from '@/lib/prisma';
+import {NextResponse} from 'next/server';
+
+export async function GET() {
+  try {
+    const tags = await prisma.tag.findMany({
+      include: {
+        news: {
+          orderBy: {createdAt: 'desc'},
+        },
+      },
+    });
+
+    const sections = tags.map((tag) => ({
+      title: tag.name,
+      cards: tag.news,
+    }));
+    console.log(sections);
+    return NextResponse.json<CardSectionInterface[]>(sections);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({error: 'Failed to fetch news'}, {status: 500});
+  }
+}
