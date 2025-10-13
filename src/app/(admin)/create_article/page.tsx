3fe54cd345777246bf51b5f92b0499552023d165
Zero_Null <boott3r@gmail.com>
2025-10-12 22:35:42 -0400

    remove editor

--- src/app/(admin)/create_article/page.tsx
+++ src/app/(admin)/create_article/page.tsx
@@ -1,11 +1,10 @@
 'use client';
-import Tiptap from './components/Editor';
 
 export default function CreateArticle() {
   return (
     <main className="bg-gray-200">
       <h1 className="text-6xl font-bold ">Create new article</h1>
-      <Tiptap />
+      TODO: Add an tiptap editor
     </main>
   );
 }
