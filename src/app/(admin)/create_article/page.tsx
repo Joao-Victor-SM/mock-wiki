049667b98d59d65645f787b4ab156f6ff63df319
Zero_Null <boott3r@gmail.com>
2025-10-12 22:14:55 -0400

    boilerplate of attempting to put tiptap

---
+++ src/app/(admin)/create_article/page.tsx
@@ -0,0 +1,10 @@
+import Tiptap from './components/Editor';
+
+export default function CreateArticle() {
+  return (
+    <main className="bg-gray-200">
+      <h1 className="text-6xl font-bold ">Create new article</h1>
+      <Tiptap />
+    </main>
+  );
+}
--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,14 +1,17 @@
 import {requireAuth} from '@/lib/authServer';
+import Link from 'next/link';
 
 export default async function Dashboard() {
-  const user = await requireAuth(); // throws redirect if not authenticated
-  console.log(user);
+  const user = await requireAuth();
   return (
-    <main>
+    <main className="p-4">
       <h1 className="text-5xl">Dashboard</h1>
       <p className="text-xl">
         Welcome, <b>{user && user.username}</b>!
       </p>
+      <Link href="/create_article" className="text-blue-600 hover:underline">
+        Create new article
+      </Link>
     </main>
   );
 }
