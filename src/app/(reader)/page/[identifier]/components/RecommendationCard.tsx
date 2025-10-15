12897348d53baf72598120919f060eb7c68f0a83
João-Victor <boott3r@gmail.com>
2025-10-15 10:19:16 -0400

    remove s

--- src/app/(reader)/page/[identifier]/components/RecommendationCard.tsx
+++ src/app/(reader)/page/[identifier]/components/RecommendationCard.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const RecommendationCard = ({identifier, title, content}: News) => {
   return (
-    <Link href={`/pages/${identifier}`}>
+    <Link href={`/page/${identifier}`}>
       <article className="p-2 border-2 border-gray-400 bg-gray-100 hover:bg-gray-200 group">
         <h2 className="font-bold text-blue-700 group-hover:underline">
           {title}
