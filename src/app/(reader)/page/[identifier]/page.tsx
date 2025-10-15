94259e93ce60e32fba5e06f2e3e4e1fada6e4409
João-Victor <boott3r@gmail.com>
2025-10-15 09:52:22 -0400

    add an title

--- src/app/(reader)/page/[identifier]/page.tsx
+++ src/app/(reader)/page/[identifier]/page.tsx
@@ -14,13 +14,23 @@ export default async function WikiPage({
   return (
     <main className="flex flex-1 gap-2 p-4 md:px-12 lg:px-24 h-full font-mono text-sm flex-col-reverse sm:flex-row justify-end sm:justify-start">
       <Aside articles={recommendations} />
+
       <article
         className="min-h-full p-4 w-full md:w-[clamp(320px,80vw,800px)] 
         border border-gray-400 bg-gray-100
         flex gap-3 justify-start font-mono text-sm text-gray-800
         flex-col shadow-md flex-1"
-        dangerouslySetInnerHTML={{__html: article?.safeContent ?? 'Not found'}}
-      />
+      >
+        <h1 className="text-lg font-bold border-b border-gray-400 pb-2 mb-2">
+          {article?.title ?? 'Untitled'}
+        </h1>
+
+        <section
+          dangerouslySetInnerHTML={{
+            __html: article?.safeContent ?? 'Not found',
+          }}
+        />
+      </article>
     </main>
   );
 }
