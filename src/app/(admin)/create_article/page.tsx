6b185dd55cd0a9dc986413b341c4f1a76e279ab6
João-Victor <boott3r@gmail.com>
2025-10-20 16:07:24 -0400

    remove references to tiptap to open space for quill

--- src/app/(admin)/create_article/page.tsx
+++ src/app/(admin)/create_article/page.tsx
@@ -4,7 +4,7 @@ export default function CreateArticle() {
   return (
     <main className="bg-gray-200">
       <h1 className="text-6xl font-bold ">Create new article</h1>
-      TODO: Add an tiptap editor
+      TODO: Add an quill editor
     </main>
   );
 }
