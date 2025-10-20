7ec264bcc0d23bad9e52db8f58b1ab9db9144668
João-Victor <boott3r@gmail.com>
2025-10-20 16:49:09 -0400

    add form base

--- src/app/(admin)/create_article/page.tsx
+++ src/app/(admin)/create_article/page.tsx
@@ -5,28 +5,40 @@ import QuillEditor from 'quill-next-react';
 
 export default function CreateArticle() {
   return (
-    <main className="bg-gray-200 p-4 flex flex-col h-screen items-start gap-2">
-      <h1 className="text-6xl font-bold ">Create new article</h1>
-      <div className="w-full">
-        <label htmlFor="title">
-          <h2 className="text-xl">Title</h2>
-        </label>
-        <input className="border border-gray-400 p-1 w-full" id="title"></input>
-      </div>
-      <h2 className="text-xl">Body</h2>
-      <QuillEditor
-        defaultValue={new Delta()}
-        config={{theme: 'next'}}
-        className="flex-1 w-full border border-gray-300 shadow"
-      />
-      <div className="flex justify-end w-full">
-        <button
-          type="button"
-          className="border border-gray-500 bg-gray-300 hover:brightness-90 p-1 cursor-pointer"
-        >
-          Save Article
-        </button>
-      </div>
+    <main className="bg-gray-200 p-4 flex flex-col h-screen items-start gap-2 ">
+      <h1 className="text-6xl font-bold">Create new article</h1>
+
+      <form className="flex flex-col w-full gap-4 h-full">
+        <div>
+          <label htmlFor="title" className="text-xl block">
+            Title
+          </label>
+          <input
+            id="title"
+            name="title"
+            type="text"
+            className="border border-gray-400 p-1 w-full"
+          />
+        </div>
+
+        <div className="flex-1 flex flex-col">
+          <label className="text-xl mb-1">Body</label>
+          <QuillEditor
+            defaultValue={new Delta()}
+            config={{theme: 'next'}}
+            className="flex-1 w-full border border-gray-300 shadow"
+          />
+        </div>
+
+        <div className="flex justify-end w-full">
+          <button
+            type="submit"
+            className="border border-gray-500 bg-gray-300 hover:brightness-90 p-1 cursor-pointer"
+          >
+            Save Article
+          </button>
+        </div>
+      </form>
     </main>
   );
 }
